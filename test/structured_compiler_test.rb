dd2b91206e20706f7332cdbcf1c07588363639e2
Tobi Lutke <tobi@shopify.com>
2026-01-15 09:52:20 -0500

    Revert structured loop to each_with_index

--- test/structured_compiler_test.rb
+++ test/structured_compiler_test.rb
@@ -8,16 +8,13 @@ class StructuredCompilerTest < Minitest::Test
     LiquidIL::Compiler::Structured.compile(source).render(assigns)
   end
 
-  def test_for_loop_compiles_to_each_do
+  def test_for_loop_compiles_to_each_with_index
     template = LiquidIL::Compiler::Structured.compile("{% for i in items %}{{ i }}{% endfor %}")
     source = template.compiled_source
 
     refute template.uses_vm, "Simple for loop should compile to structured Ruby"
     assert source, "Structured compiler should emit Ruby source"
-    assert_match(/\.each do \|__item_0__\|/, source)
-    refute_match(/each_with_index/, source, "Structured compiler should use each + manual index for YJIT-friendly loop")
-    assert_match(/__idx_0__ = 0/, source)
-    assert_match(/__idx_0__ \+= 1/, source)
+    assert_match(/\.each_with_index do \|__item_0__, __idx_0__\|/, source)
   end
 
   def test_for_loop_renders
